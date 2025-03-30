98ad6e7105147f5996ff332ce62e985a2d5252a9
unknown <phtruong44@gmail.com>
2025-03-30 09:01:10 -0700

    Added re-enable DMA upon DMA interrupt

--- Midterm/Core/Src/main.c
+++ Midterm/Core/Src/main.c
@@ -333,6 +333,10 @@ void DMA2_Stream2_IRQHandler() {
   // Clear buffer
   memset(rx_buf, 0, sizeof(rx_buf));
 
+  // Re-enable DMA
+  uint32_t* DMA_S2CR = (uint32_t*) (DMA2_BASE_ADDR + 0x10 + 0x18 * 2);
+  *DMA_S2CR |= 1;
+
   uint32_t* DMA_LIFCR = (uint32_t*)(DMA2_BASE_ADDR + 0x08);
   *DMA_LIFCR |= (1<<21);
 }
@@ -347,7 +351,7 @@ int main() {
   Button_Interrupt_Int();
   Uart_Init();
   Uart_Interrupt_Init();
-  //DMA_Uart1_RX_Init();
+  DMA_Uart1_RX_Init();
   
 
   while(1) {
